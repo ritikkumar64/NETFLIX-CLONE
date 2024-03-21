f2c3fc913b6cc23dbee008072bf061675cebedb8
ritikkumar64 <156987300+ritikkumar64@users.noreply.github.com>
2024-03-21 09:31:01 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,21 @@
+let a = "ritik"
+let b = 64
+console.log(a + b)
+// q2
+console.log(typeof (a + b))
+// q3
+const a1 = {
+  name : "ritik",
+  roll: 712,
+  isprincipal: false
+}
+a1['friend'] = "bholu"
+a1['school'] ="dav"
+console.log(a1)
+
+const dis ={
+ moanful: " expressing sorrow or grief",
+bullypulpit :"Bully pulpit refers to an important public position that allows a person to express beliefs and opinions to many people.",
+  filch:"To filch something is to secretly or casually steal it. The word filch also usually, though not always, implies that what has been stolen is small or of little monetary value",
+}
+console.log(dis.filch)
